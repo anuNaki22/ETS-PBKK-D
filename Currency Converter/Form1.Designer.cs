9c71e5d0d25f3f08b4aefc42bc4f0dd8300ed5a3
Rahmat Faris Akbar <rfarisakbar@gmail.com>
2023-04-06 17:57:28 +0700

    update

--- Currency Converter/Form1.Designer.cs
+++ Currency Converter/Form1.Designer.cs
@@ -45,43 +45,44 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightSalmon;
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label1.Location = new System.Drawing.Point(140, 4);
-            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Heavy", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(89, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(549, 67);
+            this.label1.Size = new System.Drawing.Size(279, 37);
             this.label1.TabIndex = 0;
             this.label1.Text = "Currency Converter";
             // 
             // nominal_inp
             // 
-            this.nominal_inp.Location = new System.Drawing.Point(56, 144);
-            this.nominal_inp.Margin = new System.Windows.Forms.Padding(6);
+            this.nominal_inp.Location = new System.Drawing.Point(25, 74);
             this.nominal_inp.Name = "nominal_inp";
-            this.nominal_inp.Size = new System.Drawing.Size(236, 31);
+            this.nominal_inp.Size = new System.Drawing.Size(120, 20);
             this.nominal_inp.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(48, 102);
-            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.BackColor = System.Drawing.Color.Salmon;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(21, 54);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(93, 26);
+            this.label2.Size = new System.Drawing.Size(73, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "Nominal";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(346, 102);
-            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.BackColor = System.Drawing.Color.Salmon;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(173, 54);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 26);
+            this.label3.Size = new System.Drawing.Size(42, 20);
             this.label3.TabIndex = 3;
             this.label3.Text = "Dari";
             // 
@@ -95,10 +96,9 @@
             "CNY",
             "SGD",
             "JPY"});
-            this.dari_cmb.Location = new System.Drawing.Point(354, 142);
-            this.dari_cmb.Margin = new System.Windows.Forms.Padding(6);
+            this.dari_cmb.Location = new System.Drawing.Point(177, 73);
             this.dari_cmb.Name = "dari_cmb";
-            this.dari_cmb.Size = new System.Drawing.Size(232, 33);
+            this.dari_cmb.Size = new System.Drawing.Size(118, 21);
             this.dari_cmb.TabIndex = 4;
             // 
             // hasil_txt
@@ -108,24 +108,24 @@
             this.hasil_txt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.hasil_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.hasil_txt.ForeColor = System.Drawing.Color.Goldenrod;
-            this.hasil_txt.Location = new System.Drawing.Point(38, 210);
-            this.hasil_txt.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.hasil_txt.Location = new System.Drawing.Point(19, 109);
             this.hasil_txt.Name = "hasil_txt";
-            this.hasil_txt.Size = new System.Drawing.Size(153, 67);
+            this.hasil_txt.Size = new System.Drawing.Size(86, 33);
             this.hasil_txt.TabIndex = 5;
-            this.hasil_txt.Text = "hasil";
+            this.hasil_txt.Text = "Hasil";
             this.hasil_txt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.button1.BackColor = System.Drawing.Color.Coral;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.OrangeRed;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OrangeRed;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(620, 221);
-            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Location = new System.Drawing.Point(310, 115);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(262, 83);
+            this.button1.Size = new System.Drawing.Size(131, 43);
             this.button1.TabIndex = 6;
             this.button1.Text = "Konversi";
             this.button1.UseVisualStyleBackColor = false;
@@ -141,59 +141,70 @@
             "CNY",
             "SGD",
             "JPY"});
-            this.ke_cmb.Location = new System.Drawing.Point(646, 144);
-            this.ke_cmb.Margin = new System.Windows.Forms.Padding(6);
+            this.ke_cmb.Location = new System.Drawing.Point(323, 73);
             this.ke_cmb.Name = "ke_cmb";
-            this.ke_cmb.Size = new System.Drawing.Size(232, 33);
+            this.ke_cmb.Size = new System.Drawing.Size(118, 21);
             this.ke_cmb.TabIndex = 8;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(638, 104);
-            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.BackColor = System.Drawing.Color.Salmon;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(319, 54);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(39, 26);
+            this.label4.Size = new System.Drawing.Size(30, 20);
             this.label4.TabIndex = 7;
             this.label4.Text = "Ke";
             // 
             // date_label
             // 
             this.date_label.AutoSize = true;
-            this.date_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.date_label.Location = new System.Drawing.Point(48, 290);
-            this.date_label.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.date_label.BackColor = System.Drawing.Color.Salmon;
+            this.date_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.date_label.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.date_label.Location = new System.Drawing.Point(21, 142);
             this.date_label.Name = "date_label";
-            this.date_label.Size = new System.Drawing.Size(54, 26);
+            this.date_label.Size = new System.Drawing.Size(136, 20);
             this.date_label.TabIndex = 9;
-            this.date_label.Text = "date";
+            this.date_label.Text = "conversion date";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(218, 355);
+            this.button2.BackColor = System.Drawing.Color.Orange;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.OrangeRed;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OrangeRed;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(109, 188);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(459, 64);
+            this.button2.Size = new System.Drawing.Size(230, 33);
             this.button2.TabIndex = 10;
             this.button2.Text = "Open Google Currency Converter";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // webBrowser1
             // 
-            this.webBrowser1.Location = new System.Drawing.Point(12, 425);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 222);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(10, 10);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(902, 321);
+            this.webBrowser1.Size = new System.Drawing.Size(463, 333);
             this.webBrowser1.TabIndex = 11;
             this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Menu;
-            this.ClientSize = new System.Drawing.Size(926, 758);
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.BackgroundImage = global::Currency_Converter.Properties.Resources.uang;
+            this.ClientSize = new System.Drawing.Size(463, 555);
             this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.date_label);
@@ -206,7 +217,6 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.nominal_inp);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "Currency Converter";
             this.ResumeLayout(false);
